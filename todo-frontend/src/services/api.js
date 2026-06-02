117a8835c95f79e6428f1ea18cb7eea8f1670cd9
ragini377 <ragini.panwar377@gmail.com>
2026-06-02 17:32:36 +0530

    Updated API URL

--- todo-frontend/src/services/api.js
+++ todo-frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5000/api/todos";
 
-const BASE_URL = "https://todo-app-mern-r0pa.onrender.com";
+const BASE_URL = "https://fullstackmerntodo.onrender.com";
 
 export const getTodos = () => axios.get(API_URL);
 export const createTodo = (data) => axios.post(API_URL, data);
